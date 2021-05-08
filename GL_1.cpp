fbd6778585bcfafab0f38bf483f7c16806a34dfc
StanMarek <stanislawmarek21@gmail.com>
2021-05-08 22:33:07 +0200

    New branch for classes, create directory for libs

--- GL_1.cpp
+++ GL_1.cpp
@@ -9,7 +9,7 @@
 
 #define M_PI 3.14159
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "lib\stb_image.h"
 #include "shaderClass.h"
 #include "VAOClass.h"
 
@@ -290,7 +290,7 @@ int main() {
 		}
 
 		setCameraKeyboard(uniView, time.asMicroseconds());
-		
+
 		// Nadanie scenie koloru czarnego
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -538,36 +538,31 @@ void setCameraKeyboard(GLint uniView, float elapsedTime) {
 void stereoProjection(GLuint shaderProgram_, float _left, float _right, float _bottom, float _top, float _near, float _far, float _zero_plane, float _dist, float _eye)
 {
 	/*
-	    Perform the perspective projection for one eye's subfield.
-	    The projection is in the direction of the negative z-axis.
-	            _left=-6.0;
-	            _right=6.0;
-	            _bottom=-4.8;
-	             _top=4.8;
-	    [default: -6.0, 6.0, -4.8, 4.8]
-	    left, right, bottom, top = the coordinate range, in the plane of zero parallax setting,
-	         which will be displayed on the screen.
-	         The ratio between (right-left) and (top-bottom) should equal the aspect
-	    ratio of the display.
-
-
-	                  _near=6.0;
-	                  _far=-20.0;
-	    [default: 6.0, -6.0]
-	    near, far = the z-coordinate values of the clipping planes.
-
-	                  _zero_plane=0.0;
-	    [default: 0.0]
-	    zero_plane = the z-coordinate of the plane of zero parallax setting.
-
-	    [default: 14.5]
-	                     _dist=10.5;
+		Perform the perspective projection for one eye's subfield.
+		The projection is in the direction of the negative z-axis.
+				_left=-6.0;
+				_right=6.0;
+				_bottom=-4.8;
+				 _top=4.8;
+		[default: -6.0, 6.0, -4.8, 4.8]
+		left, right, bottom, top = the coordinate range, in the plane of zero parallax setting,
+			 which will be displayed on the screen.
+			 The ratio between (right-left) and (top-bottom) should equal the aspect
+		ratio of the display.
+					  _near=6.0;
+					  _far=-20.0;
+		[default: 6.0, -6.0]
+		near, far = the z-coordinate values of the clipping planes.
+					  _zero_plane=0.0;
+		[default: 0.0]
+		zero_plane = the z-coordinate of the plane of zero parallax setting.
+		[default: 14.5]
+						 _dist=10.5;
 	   dist = the distance from the center of projection to the plane of zero parallax.
-
-	    [default: -0.3]
-	                 _eye=-0.3;
-	    eye = half the eye separation; positive for the right eye subfield,
-	    negative for the left eye subfield.
+		[default: -0.3]
+					 _eye=-0.3;
+		eye = half the eye separation; positive for the right eye subfield,
+		negative for the left eye subfield.
 	*/
 
 	float   _dx = _right - _left;
